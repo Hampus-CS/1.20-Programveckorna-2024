cf27ab3100b706b2ddaf68665f6ec4543909cd91
Elmer-Sunnanbo <elmer.sunnanbo@elev.ga.lbs.se>
2024-01-10 16:24:42 +0100

    Byggde om PlayerMovement
    Mindre buggigt, lite annorlunda, tog bort squash and stretch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public GameObject sprite;
     bool grounded = false;
     float speed = 0f;
+    int Grounds = 0;
+    [SerializeField] float PlayerSpeed;
+    [SerializeField] float PlayerJumpHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -17,16 +20,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speed += ((Input.GetAxisRaw("Horizontal") * 4f) - speed) * 0.035f;
+        speed = 0;
+        //speed += ((Input.GetAxisRaw("Horizontal") * 4f) - speed) * 0.035f;
+        if (Input.GetKey(KeyCode.D))
+        {
+            speed++;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            speed--;
+        }
 
-        rb.velocity = new Vector2(speed, rb.velocity.y);
+        rb.velocity = new Vector2(speed * PlayerSpeed, rb.velocity.y);
+
+
 
         if (Physics2D.Raycast(transform.position, Vector2.down, 0.25f, mask))
         {
             if (grounded == false)
             {
-                sprite.GetComponent<Scale>().scale_x = 2f;
-                sprite.GetComponent<Scale>().scale_y = 0.25f;
+                //sprite.GetComponent<Scale>().scale_x = 2f;
+                //sprite.GetComponent<Scale>().scale_y = 0.25f;
             }
 
             grounded = true;
@@ -36,17 +50,27 @@ public class PlayerMovement : MonoBehaviour
             grounded = false;
         }
 
-        if (Input.GetKeyUp("w") && grounded)
+        if (Input.GetKeyDown(KeyCode.Space) && Grounds !=0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + 15f);
+            rb.velocity = new Vector2(rb.velocity.x, PlayerJumpHeight);
 
-            sprite.GetComponent<Scale>().scale_x = 0.25f;
-            sprite.GetComponent<Scale>().scale_y = 2f;
+            //sprite.GetComponent<Scale>().scale_x = 0.25f;
+            //sprite.GetComponent<Scale>().scale_y = 2f;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
+        if (collision.gameObject.GetComponent<GroundScript>())
+        {
+            Grounds++;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<GroundScript>())
+        {
+            Grounds--;
+        }
     }
 }
