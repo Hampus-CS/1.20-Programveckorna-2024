bb7308b82a89a68ac65b79669ae3b06109e0dba5
Elmer-Sunnanbo <elmer.sunnanbo@elev.ga.lbs.se>
2024-01-10 13:49:09 +0100

    Lade till item tracker
    Item Tracker är ett skript på spelaren med en public static int variabel i.
    Varje item har ett id, använd variabeln för att modifiera eller läsa ut vilket item spelaren har.

---
+++ Assets/Scripts/ItemTracker.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTracker : MonoBehaviour
+{
+    //CurrentItemID är variabeln som används för att hålla koll på vilket item spelaren håller i.
+    public static int CurrentItemID;
+    //Item ID list: 
+    //Fists: 0
+    //Bat: 1
+    void Start()
+    {
+        CurrentItemID = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
