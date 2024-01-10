f1779da34b2e82ff7d27425479ebf152ae01f00f
Elmer-Sunnanbo <elmer.sunnanbo@elev.ga.lbs.se>
2024-01-10 15:06:02 +0100

    Lade till att kunna plocka upp items, och ett baseballträ
    Items blir blå när du är nära, och kan plockas upp med E

--- Assets/Scripts/ItemTracker.cs
+++ Assets/Scripts/ItemTracker.cs
@@ -17,6 +17,9 @@ public class ItemTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log(CurrentItemID);
+        }
     }
 }
