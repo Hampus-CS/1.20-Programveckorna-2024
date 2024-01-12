00bf91105a1f5a8d0caf8d5241014958b9078e3e
Hampus Carlström Svanberg <150423568+Hampus-CS@users.noreply.github.com>
2024-01-12 16:25:57 +0100

    Tile go thru (Not finished) (#17)
    * First addition
    
    Requeirs new code
    
    * Move of items into folder

--- Assets/Scripts/RoomTeleporter.cs
+++ Assets/Scripts/RoomTeleporter.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class RoomTeleporter : MonoBehaviour
 {
+    // Generates a list, the list you can fill from unity.
     public List<Transform> rooms = new List<Transform>();
     private List<Transform> availableRooms = new List<Transform>();
     public bool isNearDoor = false;
 
+    // Checks inputs the rooms that was added in unity in to the list.
     void Start()
     {
         availableRooms.AddRange(rooms);
     }
 
+    // Allows the player to teleport to a room if there are rooms left, the player is near a door and then waits for player to press T.
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T) && availableRooms.Count > 0 && isNearDoor)
@@ -21,6 +24,7 @@ public class RoomTeleporter : MonoBehaviour
         }
     }
 
+    // Randomizes what room you will get when you activate a door, transports the player to the selected room and then removes the room from selection.
     void TeleportPlayerToRandomRoom()
     {
         int randomIndex = Random.Range(0, availableRooms.Count);
