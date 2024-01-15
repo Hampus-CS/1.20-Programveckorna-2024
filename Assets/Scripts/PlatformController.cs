09fb58f945382f38373442774d147fd42e1c6548
Hampe007 <150423568+Hampus-CS@users.noreply.github.com>
2024-01-16 00:06:51 +0100

    Main Menu updated, Pause Menu added, Options Menu added
    Main Menu - Got a well deserved temporary upgrade.
    Options menu - Added for resolution, audio, fullscreen and such.
    Pause Menu - Does what it says.

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -6,50 +6,7 @@ public class PlatformController : MonoBehaviour
 {
     public Collider2D platformCollider;
     public Transform player;
-    /*
-    private void Start()
-    {
-        platformCollider1 = GetComponent<Collider2D>();
-        platformCollider2 = GetComponent<Collider2D>();
-        DisablePlatformCollider();
-    }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player entered the platform trigger zone.");
-            EnablePlatformCollider();
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player exited the platform trigger zone.");
-            DisablePlatformCollider();
-        }
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.S) && (platformCollider2.enabled == true))
-        {
-            FallThroughPlatform();
-        }
-    }
-
-    private void FallThroughPlatform()
-    {
-        // Check if the player is currently on the platform
-        if (platformCollider2.enabled)
-        {
-            DisablePlatformCollider();
-            //Invoke("EnablePlatformCollider", 0.1f); // Delayed re-enable to prevent immediate re-entry
-        }
-    }
-    */
     private void Update()
     {
         if (GameObject.FindGameObjectWithTag("Player"))
@@ -78,13 +35,13 @@ public class PlatformController : MonoBehaviour
 
     private void EnablePlatformCollider()
     {
-        Debug.Log("True");
+        //Debug.Log("True");
         platformCollider.enabled = true;
     }
 
     private void DisablePlatformCollider()
     {
-        Debug.Log("False");
+        //Debug.Log("False");
         platformCollider.enabled = false;
 
     }
