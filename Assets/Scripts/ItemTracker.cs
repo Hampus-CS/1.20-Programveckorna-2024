36e7b5132725a7f500a60863eb83121b3a7bf3ff
Elmer-Sunnanbo <150426611+Elmer-Sunnanbo@users.noreply.github.com>
2024-01-16 15:46:22 +0100

    Player can now throw things (#32)
    That's all man

--- Assets/Scripts/ItemTracker.cs
+++ Assets/Scripts/ItemTracker.cs
@@ -7,6 +7,8 @@ public class ItemTracker : MonoBehaviour
     //CurrentItemID är variabeln som används för att hålla koll på vilket item spelaren håller i.
     public static int CurrentItemID;
     public static int CurrentItemDurability = 0;
+    public static bool Delay;
+    public static int Timer;
     //Item ID list: 
     //Fists: 0
     //Bat: 1
@@ -18,6 +20,14 @@ public class ItemTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Timer > 0)
+        {
+            Timer--;
+        }
+        if (Timer == 0)
+        {
+            Delay = false;
+        }
 
     }
 }
