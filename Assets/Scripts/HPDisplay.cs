ea4f71006d76353dc3262ab8beb304fed79aa413
Elmer-Sunnanbo <elmer.sunnanbo@elev.ga.lbs.se>
2024-01-18 15:59:03 +0100

    Added UI For health and Current item

---
+++ Assets/Scripts/HPDisplay.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPDisplay : MonoBehaviour
+{
+    [SerializeField] Sprite HP5;
+    [SerializeField] Sprite HP4;
+    [SerializeField] Sprite HP3;
+    [SerializeField] Sprite HP2;
+    [SerializeField] Sprite HP1;
+    [SerializeField] Image TheImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerMovement.PlayerHealth == 5)
+        {
+            TheImg.sprite = HP5;
+        }
+        if (PlayerMovement.PlayerHealth == 4)
+        {
+            TheImg.sprite = HP4;
+        }
+        if (PlayerMovement.PlayerHealth == 3)
+        {
+            TheImg.sprite = HP3;
+        }
+        if (PlayerMovement.PlayerHealth == 2)
+        {
+            TheImg.sprite = HP2;
+        }
+        if (PlayerMovement.PlayerHealth == 1)
+        {
+            TheImg.sprite = HP1;
+        }
+    }
+}
---
+++ Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDisplay : MonoBehaviour
+{
+    [SerializeField] Sprite Fists;
+    [SerializeField] Sprite Bat2;
+    [SerializeField] Sprite Bat1;
+    [SerializeField] Sprite Knife1;
+    [SerializeField] Image TheImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ItemTracker.CurrentItemID == 0)
+        {
+            TheImg.sprite = Fists;
+        }
+        if (ItemTracker.CurrentItemID == 1)
+        {
+            if(ItemTracker.CurrentItemDurability == 2)
+            {
+                TheImg.sprite = Bat2;
+            }
+            else
+            {
+                TheImg.sprite = Bat1;
+            }
+        }
+
+        if (ItemTracker.CurrentItemID == 2)
+        {
+            TheImg.sprite = Knife1;
+        }
+    }
+}
