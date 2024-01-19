a992523763c2ce047ae4f25f1d77b94786863f8d
Hampe007 <150423568+Hampus-CS@users.noreply.github.com>
2024-01-19 09:16:49 +0100

    Music controllers and background modification

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -8,25 +8,29 @@ using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-
     public AudioMixer audioMixer;
+    public AudioMixer gameAudioMixer;
+    private const string VolumePrefsKey = "Volume";
+    private const string GameVolumePrefsKey = "GameVolume";
 
     public TMPro.TMP_Dropdown resolutionDropdown;
-
     Resolution[] resolutions;
 
-    void Start ()
+    void Start()
     {
-        
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
-        
-        List<string> options = new List<string>();
-        
-        int currentResolutionIndex = 0;
-        for(int i = 0; i < resolutions.Length; i++)
-        {
 
+        List<string> options = new List<string>();
+
+        float savedVolume = PlayerPrefs.GetFloat(VolumePrefsKey, 1.0f);
+        float savedGameVolume = PlayerPrefs.GetFloat(GameVolumePrefsKey, 1.0f);
+        SetVolume(savedVolume);
+        SetGameVolume(savedGameVolume);
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
 
@@ -34,45 +38,45 @@ public class SettingsMenu : MonoBehaviour
             {
                 currentResolutionIndex = i;
             }
-
         }
 
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
-
     }
 
-    public void SetResolution (int resolutionIndex)
+    public void SetResolution(int resolutionIndex)
     {
-
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
+    public void SetVolume(float volume)
+    {
+        if (audioMixer != null)
+        {
+            audioMixer.SetFloat("volume", volume);
+        }
+        PlayerPrefs.SetFloat(VolumePrefsKey, volume);
     }
 
 
-    public void SetVolume (float volume)
+    public void SetGameVolume(float gameVolume)
     {
-
-        audioMixer.SetFloat("volume", volume);
-        //Debug.Log(volume);
-
+        if (gameAudioMixer != null)
+        {
+            gameAudioMixer.SetFloat("GameVolume", gameVolume);
+        }
+        PlayerPrefs.SetFloat(GameVolumePrefsKey, gameVolume);
     }
 
-    public void SetQuality (int qualityIndex)
+    public void SetQuality(int qualityIndex)
     {
-
         QualitySettings.SetQualityLevel(qualityIndex);
-
     }
 
-    public void SetFullscreen (bool isFullscreen)
+    public void SetFullscreen(bool isFullscreen)
     {
-
         Screen.fullScreen = isFullscreen;
-
     }
-
-
 }
