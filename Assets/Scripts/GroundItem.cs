a02d244090376a056004bdcd3c77c35669851fe0
Hampus-CS <150423568+Hampus-CS@users.noreply.github.com>
2024-01-30 15:18:10 +0100

    A reupload of a main branch
    A reupload of a main branch clone with some crucial changes to allow for the SFX branch to be merged.

--- Assets/Scripts/GroundItem.cs
+++ Assets/Scripts/GroundItem.cs
@@ -7,25 +7,23 @@ public class GroundItem : MonoBehaviour
 {
     bool PlayerOnItem = false;
     SpriteRenderer TheSR;
-    public int ThisItemID = 1;
+    [SerializeField] public int ThisItemID;
     [SerializeField] Sprite BatSprite;
     [SerializeField] Sprite KnifeSprite;
+
+    [SerializeField] AudioSource batPickUpSource;
+    [SerializeField] AudioSource knifePickUpSource;
     //IDs:
     //1: Bat
     //2: Knife
     void Start()
     {
-        if (Random.Range(0f, 100f) <= 15f)
-        {
-            ThisItemID = 2;
-        }
-
         TheSR = gameObject.GetComponent<SpriteRenderer>();
-        if(ThisItemID == 1)
+        if (ThisItemID == 1)
         {
             TheSR.sprite = BatSprite;
         }
-        if(ThisItemID == 2)
+        if (ThisItemID == 2)
         {
             TheSR.sprite = KnifeSprite;
         }
@@ -34,27 +32,35 @@ public class GroundItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && PlayerOnItem)
+        if (Input.GetKeyDown(KeyCode.E) && PlayerOnItem && ThisItemID != 0)
         {
-            Destroy(gameObject);
+
+            TheSR.color = new Color(1, 1, 1, 0);
             ItemTracker.CurrentItemID = ThisItemID;
             ItemTracker.Timer = 5;
             ItemTracker.Delay = true;
+
             if (ItemTracker.CurrentItemID == 1)
             {
                 ItemTracker.CurrentItemDurability = 2;
+
+                batPickUpSource.Play();
             }
             if (ItemTracker.CurrentItemID == 2)
             {
-                ItemTracker.CurrentItemDurability = 1;    
+                ItemTracker.CurrentItemDurability = 1;
+
+                knifePickUpSource.Play();
             }
+            ThisItemID = 0;
+            Destroy(gameObject, 2);
         }
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && ThisItemID != 0)
         {
             PlayerOnItem = true;
             TheSR.color = new Color(0.5f, 0.5f, 1);
@@ -62,7 +68,7 @@ public class GroundItem : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && ThisItemID != 0)
         {
             PlayerOnItem = false;
             TheSR.color = new Color(1, 1, 1);
